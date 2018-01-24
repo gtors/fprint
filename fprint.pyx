23f6c4724fc7160f58d26c7cca9510489b180e09
gtors <gtors.potato@gmail.com>
2018-01-25 01:34:52 +0300

    Null safety

--- fprint.pyx
+++ fprint.pyx
@@ -13,29 +13,48 @@ cdef class Driver:
 
     @property
     def name(self):
-        return <bytes> fp_driver_get_name(self.ptr)
+        if self.ptr != NULL:
+            return <bytes> fp_driver_get_name(self.ptr)
 
     @property
     def full_name(self):
-        return <bytes> fp_driver_get_full_name(self.ptr)
+        if self.ptr != NULL:
+            return <bytes> fp_driver_get_full_name(self.ptr)
 
     @property
     def driver_id(self):
-        return fp_driver_get_driver_id(self.ptr)
+        if self.ptr != NULL:
+            return fp_driver_get_driver_id(self.ptr)
 
     @property
     def scan_type(self):
-        return fp_driver_get_scan_type(self.ptr)
+        if self.ptr != NULL:
+            return fp_driver_get_scan_type(self.ptr)
 
 
 cdef class DiscoveredPrints:
     cdef fp_dscv_print **prints
+    cdef int number_prints
 
     def __cinit__(self):
         self.prints = fp_discover_prints()
 
+        cdef int i = 0
+        while self.prints[i] != NULL:
+            i += 1
+        self.number_prints = i
+
     def __dealloc__(self):
         fp_dscv_prints_free(self.prints)
+
+    def __getitem__(self, int i):
+        cdef fp_dscv_print *p 
+        if i < self.number_prints:
+            p = self.prints[i]
+            return DiscoveredPrint.new(p)
+
+    def __len__(self):
+        return self.number_prints
 
 
 cdef class PrintData:
@@ -50,63 +69,72 @@ cdef class PrintData:
         pd = PrintData()
         if fp_print_data_load(d.ptr, finger, &pd.ptr) > 0:
             return pd
-        else:
-            return None
 
     @staticmethod
     def from_discovered_print(DiscoveredPrint p):
         pd = PrintData()
         if fp_print_data_from_dscv_print(p.ptr, &pd.ptr) > 0:
             return pd
-        else:
-            return None
 
     @staticmethod
     def from_data(bytes data):
         pd = PrintData()
         pd.ptr = fp_print_data_from_data(data, len(data))
-        if pd.ptr == NULL:
-            return None
-        else:
+        if pd.ptr != NULL:
             return pd
 
     @property
     def driver_id(self):
-        return fp_print_data_get_driver_id(self.ptr)
+        if self.ptr != NULL:
+            return fp_print_data_get_driver_id(self.ptr)
 
     @property
     def devtype(self):
-        return fp_print_data_get_devtype(self.ptr)
+        if self.ptr != NULL:
+            return fp_print_data_get_devtype(self.ptr)
 
     @property
     def data(self):
-        cdef unsigned char *buf = NULL
-        fp_print_data_get_data(self.ptr, &buf)
-        return <bytes> buf
+        cdef unsigned char *buf
+        if self.ptr != NULL:
+            fp_print_data_get_data(self.ptr, &buf)
+            return <bytes> buf
 
     def save(self, fp_finger finger):
-        return fp_print_data_save(self.ptr, finger)
+        if self.ptr != NULL:
+            return fp_print_data_save(self.ptr, finger)
 
     def delete(self, Device d, fp_finger finger):
-        return fp_print_data_delete(d.ptr, finger)
+        if self.ptr != NULL:
+            return fp_print_data_delete(d.ptr, finger)
 
 
 cdef class DiscoveredPrint:
     cdef fp_dscv_print *ptr
 
+    @staticmethod
+    cdef new(fp_dscv_print *ptr):
+        p = DiscoveredPrint()
+        p.ptr = ptr
+        return p
+
     @property
     def driver_id(self):
-        return fp_dscv_print_get_driver_id(self.ptr)
+        if self.ptr != NULL:
+            return fp_dscv_print_get_driver_id(self.ptr)
 
     @property
     def devtype(self):
-        return fp_dscv_print_get_devtype(self.ptr)
+        if self.ptr != NULL:
+            return fp_dscv_print_get_devtype(self.ptr)
 
     def get_finger(self):
-        return fp_dscv_print_get_finger(self.ptr)
+        if self.ptr != NULL:
+            return fp_dscv_print_get_finger(self.ptr)
 
     def delete(self):
-        return fp_dscv_print_delete(self.ptr)
+        if self.ptr != NULL:
+            return fp_dscv_print_delete(self.ptr)
 
 
 cdef class Minutia:
@@ -120,51 +148,63 @@ cdef class Minutia:
 
     @property
     def x(self):
-        return self.ptr[0].x
+        if self.ptr != NULL:
+            return self.ptr[0].x
 
     @property
     def y(self):
-        return self.ptr[0].y
+        if self.ptr != NULL:
+            return self.ptr[0].y
     
     @property
     def ex(self):
-        return self.ptr[0].ex
+        if self.ptr != NULL:
+            return self.ptr[0].ex
     
     @property
     def ey(self):
-        return self.ptr[0].ey
+        if self.ptr != NULL:
+            return self.ptr[0].ey
     
     @property
     def direction(self):
-        return self.ptr[0].direction
+        if self.ptr != NULL:
+            return self.ptr[0].direction
     
     @property
     def reliability(self):
-        return self.ptr[0].reliability
+        if self.ptr != NULL:
+            return self.ptr[0].reliability
     
     @property
     def type(self):
-        return self.ptr[0].type
+        if self.ptr != NULL:
+            return self.ptr[0].type
     
     @property
     def appearing(self):
-        return self.ptr[0].appearing
+        if self.ptr != NULL:
+            return self.ptr[0].appearing
     
     @property
     def feature_id(self):
-        return self.ptr[0].feature_id
+        if self.ptr != NULL:
+            return self.ptr[0].feature_id
     
     @property
     def nbrs(self):
-        return self.ptr[0].nbrs[0]
+        if self.ptr != NULL:
+            return self.ptr[0].nbrs[0]
     
     @property
     def ridge_counts(self):
-        return self.ptr[0].ridge_counts[0]
+        if self.ptr != NULL:
+            return self.ptr[0].ridge_counts[0]
     
     @property
     def num_nbrs(self):
-        return self.ptr[0].num_nbrs
+        if self.ptr != NULL:
+            return self.ptr[0].num_nbrs
 
 
 cdef class Image:
@@ -182,39 +222,43 @@ cdef class Image:
 
     @property
     def width(self):
-        return fp_img_get_width(self.ptr)
+        if self.ptr != NULL:
+            return fp_img_get_width(self.ptr)
 
     @property
     def height(self):
-        return fp_img_get_height(self.ptr)
+        if self.ptr != NULL:
+            return fp_img_get_height(self.ptr)
 
     @property
     def data(self):
-        return <bytes> fp_img_get_data(self.ptr)
+        if self.ptr != NULL:
+            return <bytes> fp_img_get_data(self.ptr)
 
     def save_to_file(self, str path):
         cdef bytes py_bytes = path.encode()
         cdef char* c_string = py_bytes
-        return fp_img_save_to_file(self.ptr, c_string) 
+        if self.ptr != NULL:
+            return fp_img_save_to_file(self.ptr, c_string) 
 
     def standartize(self):
-        fp_img_standardize(self.ptr)
+        if self.ptr != NULL:
+            fp_img_standardize(self.ptr)
 
     def binarize(self):
-        cdef fp_img *img = fp_img_binarize(self.ptr)
-        if img == NULL:
-            return None
-        else:
-            return Image.new(img)
+        cdef fp_img *img
+        if self.ptr != NULL:
+            img = fp_img_binarize(self.ptr)
+            if img != NULL:
+                return Image.new(img)
 
     def get_minutiae(self):
         cdef int nr_minutiae = 0
-        cdef fp_minutia **minutiae = fp_img_get_minutiae(self.ptr, &nr_minutiae)
-
-        if minutiae == NULL:
-            return None
-        else:
-            return tuple(Minutia.new(minutiae[i]) for i in xrange(nr_minutiae))
+        cdef fp_minutia **minutiae
+        if self.ptr != NULL:
+            minutiae = fp_img_get_minutiae(self.ptr, &nr_minutiae)
+            if minutiae != NULL:
+                return tuple(Minutia.new(minutiae[i]) for i in xrange(nr_minutiae))
 
 
 cdef class DiscoveredDevices:
@@ -269,21 +313,24 @@ cdef class DiscoverdDevice:
 
     @property
     def driver(self):
-        cdef fp_driver *drv = fp_dscv_dev_get_driver(self.ptr)
-        if drv == NULL:
-            return None
-        else:
-            return Driver.new(drv)
+        cdef fp_driver *drv
+        if self.ptr != NULL:
+            drv = fp_dscv_dev_get_driver(self.ptr)
+            if drv != NULL:
+                return Driver.new(drv)
 
     @property
     def devtype(self):
-        return fp_dscv_dev_get_devtype(self.ptr)
+        if self.ptr != NULL:
+            return fp_dscv_dev_get_devtype(self.ptr)
 
     def supports_print_data(self, PrintData pd):
-        return <bint> fp_dscv_dev_supports_print_data(self.ptr, pd.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dscv_dev_supports_print_data(self.ptr, pd.ptr)
 
     def supports_dscv_print(self, DiscoveredPrint p):
-        return <bint> fp_dscv_dev_supports_dscv_print(self.ptr, p.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dscv_dev_supports_dscv_print(self.ptr, p.ptr)
 
 
 cdef class Device:
@@ -298,71 +345,82 @@ cdef class Device:
     @staticmethod
     def open(DiscoverdDevice dd):
         cdef fp_dev *dev = fp_dev_open(dd.ptr)
-        if dev == NULL:
-            return None
-        else:
+        if dev != NULL:
             return Device.new(dev)
 
     def close(self):
-        fp_dev_close(self.ptr)
+        if self.ptr != NULL:
+            fp_dev_close(self.ptr)
 
     @property
     def driver(self):
-        cdef fp_driver *drv = fp_dev_get_driver(self.ptr)
-        if drv == NULL:
-            return None
-        else:
-            return Driver.new(drv)
+        cdef fp_driver *drv
+        if self.ptr != NULL:
+            drv = fp_dev_get_driver(self.ptr)
+            if drv != NULL:
+                return Driver.new(drv)
 
     @property
     def devtype(self):
-        return fp_dev_get_devtype(self.ptr)
+        if self.ptr != NULL:
+            return fp_dev_get_devtype(self.ptr)
 
     @property
     def number_enroll_stages(self):
-        return fp_dev_get_nr_enroll_stages(self.ptr)
+        if self.ptr != NULL:
+            return fp_dev_get_nr_enroll_stages(self.ptr)
 
     def supports_print_data(self, PrintData pd):
-        return <bint> fp_dev_supports_print_data(self.ptr, pd.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dev_supports_print_data(self.ptr, pd.ptr)
 
     def supports_dscv_print(self, DiscoveredPrint p):
-        return <bint> fp_dev_supports_dscv_print(self.ptr, p.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dev_supports_dscv_print(self.ptr, p.ptr)
 
     def supports_imaging(self):
-        return <bint> fp_dev_supports_imaging(self.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dev_supports_imaging(self.ptr)
 
     def supports_identification(self):
-        return <bint> fp_dev_supports_identification(self.ptr)
+        if self.ptr != NULL:
+            return <bint> fp_dev_supports_identification(self.ptr)
 
     def capture_image(self, int unconditional):
-        i = Image()
-        fp_dev_img_capture(self.ptr, unconditional, &i.ptr)
-        return i
+        if self.ptr != NULL:
+            i = Image()
+            fp_dev_img_capture(self.ptr, unconditional, &i.ptr)
+            return i
 
     def image_width(self):
-        return fp_dev_get_img_width(self.ptr)
+        if self.ptr != NULL:
+            return fp_dev_get_img_width(self.ptr)
 
     def image_height(self):
-        return fp_dev_get_img_height(self.ptr)
+        if self.ptr != NULL:
+            return fp_dev_get_img_height(self.ptr)
 
     def enroll_finger(self):
-        pd = PrintData()
-        i = Image()
-        fp_enroll_finger_img(self.ptr, &pd.ptr, &i.ptr)
-        return (pd, i)
+        if self.ptr != NULL:
+            pd = PrintData()
+            i = Image()
+            fp_enroll_finger_img(self.ptr, &pd.ptr, &i.ptr)
+            return (pd, i)
 
     def verify_finger(self):
-        pd = PrintData()
-        i = Image()
-        fp_verify_finger_img(self.ptr, pd.ptr, &i.ptr)
-        return (pd, i)
+        if self.ptr != NULL:
+            pd = PrintData()
+            i = Image()
+            fp_verify_finger_img(self.ptr, pd.ptr, &i.ptr)
+            return (pd, i)
 
     def identify_finger(self):
         cdef size_t match_offset = 0
-        pd = PrintData()
-        i = Image()
-        fp_identify_finger_img(self.ptr, &pd.ptr, &match_offset, &i.ptr)
-        return (pd, match_offset, i)
+        if self.ptr != NULL:
+            pd = PrintData()
+            i = Image()
+            fp_identify_finger_img(self.ptr, &pd.ptr, &match_offset, &i.ptr)
+            return (pd, match_offset, i)
 
 
 #cdef class Poll:
